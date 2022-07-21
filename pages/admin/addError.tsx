7fdf863a52cc0cd2577593bf46404e7d9a924aa3
alidauda <alidauda14@gmail.com>
2022-07-21 22:11:17 +0100

    error page done

--- pages/admin/addError.tsx
+++ pages/admin/addError.tsx
@@ -1,11 +1,79 @@
 import { NextPage } from "next";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Textarea,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+import { useAuth } from "../../lib/firebase";
+import axios from "axios";
 
 const AddError: NextPage = () => {
-    return(
-        <>
-        add error
-        </>
-    )
-}
+    const [error,setError] = useState("")
+    const [fixed,fixedError] = useState("")
+    const auth= useAuth();
+    const [loading,loadingError] = useState(true)
+    const id = auth?.userId?.uid
+
+    async function Caller(errorName: string, id: string, fixed: string) {
+        await axios
+          .post("/api/errorHelper", {
+            code: errorName,
+            errorName,
+            id,
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      
+        await axios
+          .post("/api/fixHelper", {
+            codeFix: fixed,
+      
+            id,
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+  return (
+    <Box width="100%">
+      <Box maxW="1200px" m="auto" display="flex">
+       
+          <FormLabel m="3px">ErrorCode</FormLabel>
+          <Textarea placeholder="paste your error code" m="3px"  onChange={(e)=>{
+           
+            setError(e.target.value);
+          }}/>
+          <FormLabel m="3px">CodeFix</FormLabel>
+          <Textarea placeholder="paste the fixed code here" m="3px"  onChange={(e)=>{
+             
+             fixedError(e.target.value);
+          }}/>
+          <Button colorScheme="teal" variant="solid" ml="50%" mt="10px" alignSelf="center" isLoading={loading} onSubmit={(e)=>{
+             console.log("Loading");
+            loadingError(true)
+            Caller(error,id!,fixed)
+            loadingError(false)}
+           
+           
+            }>
+
+            Button
+          </Button>
+       
+      </Box>
+    </Box>
+  );
+};
 
 export default AddError;
